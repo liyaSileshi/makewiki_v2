8fa6c98a4fb3ffbab7296471a2d694fcb95a9c69
liyaSileshi <liyasileshi@gmail.com>
2019-11-12 21:17:43 -0800

    forgot password added

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -136,6 +136,9 @@ WIKI_PAGE_TITLE_MAX_LENGTH = 600
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 DEFAULT_LOGOUT_URL = '/'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
 # Required for Heroku
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
