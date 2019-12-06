0ab8719ebdbdb2af982515f8867de4e0622e4e22
liyaSileshi <liyasileshi@gmail.com>
2019-12-05 16:04:37 -0800

    heroku

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-  ALLOWED_HOSTS = ['localhost', 'makewiki-v2-lt.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'makewiki-v2-lt.herokuapp.com']
 
 
 
